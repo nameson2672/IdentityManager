ee732bce37070d904ddc7f6b4b19b332fd1b1c40
Nameson Gaudel <namesongaudel.ng@gmail.com>
2022-01-29 07:43:54 +0545

    Added database connection and some controller in stater file

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using IdentityManager.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace IdentityManager.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> Register()
+        {
+            RegisterViewModel registerViewModel = new RegisterViewModel();
+
+            return View(registerViewModel);
+        }
+    }
+}
